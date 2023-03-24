65eacd3caa6db019bb15cdf3aea9016841cd1cfb
mdkhanga <khangaonkar@gmail.com>
2023-03-24 15:32:00 -0700

    leetcode 121

---
+++ leetcode/src/main/java/com/mj/leetcode/BestTimeToBuySellStock.java
@@ -0,0 +1,9 @@
+package com.mj.leetcode;
+
+public class BestTimeToBuySellStock {
+
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+}
---
+++ leetcode/src/test/java/com/mj/leetcode/BestTimeToBuySellStockTest.java
@@ -0,0 +1,16 @@
+package com.mj.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BestTimeToBuySellStockTest {
+
+    BestTimeToBuySellStock bs = new BestTimeToBuySellStock();
+
+    @Test
+    public void test1() {
+        
+    }
+
+}
