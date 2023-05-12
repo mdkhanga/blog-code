9b2033c3a3acafa2490e9f19a74398eb2199bbd3
mdkhanga <khangaonkar@gmail.com>
2023-05-12 16:47:21 -0700

    Leetcode 86 started

---
+++ leetcode/src/main/java/com/mj/leetcode/PartitionList.java
@@ -0,0 +1,17 @@
+package com.mj.leetcode;
+
+public class PartitionList {
+
+    public ListNode partition(ListNode head, int x) {
+
+        return null;
+    }
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
